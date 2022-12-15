0ee3e278fd1cb7a62d1a787339f8ab1b8e910310
RonnieMT <ronnieronton@me.com>
2022-12-14 19:11:42 -0700

    Finsihed day01

--- day01.py
+++ day01.py
@@ -1,1 +1,25 @@
-print("hello world")
+with open("day01text.txt") as f:
+ 
+    elfNum = 1
+    finalElfNum = 1
+    maxCals = 0
+    tempCals = 0
+    elfList = []
+    i = 0
+    for line in f.readlines():
+        #print(line)
+        if(line == "\n"):
+            if(maxCals < tempCals):
+                maxCals = tempCals
+                finalElfNum = elfNum
+            elfList.append(tempCals)
+            i += 1
+            tempCals = 0
+            elfNum += 1
+            continue
+        #print(tempCals)
+        tempCals += int(line)
+    print(maxCals)
+
+    maxElfCals = sorted(elfList)
+    print(maxElfCals[len(maxElfCals)-1] + maxElfCals[len(maxElfCals)-2] + maxElfCals[len(maxElfCals)-3])
