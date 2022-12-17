7c8566e2156d24c691897073688b94d39cc59a5e
RonnieMT <ronnieronton@me.com>
2022-12-17 01:59:55 -0700

    Finsihed day03

--- day03.py
+++ day03.py
@@ -13,6 +13,22 @@ with open("day03input.txt") as f:
     #    else:
     #        points += 1 + ord(common) - 97
 
+    #print(len(f.readlines()))
+    
+    inputFile = f.read().splitlines()
+    for i in range(0, len(inputFile)-1, 3):
+        string1 = inputFile[i]
+        string2 = inputFile[i+1]
+        string3 = inputFile[i+2]
+        common = set(string1).intersection(string2)
+        common = set(common).intersection(string3)
+        common = list(common)
+        if(ord(common[0]) < 97):
+           # add 27 because thats the points that capital letters start at, and subtract 65 to 
+           # figure out what char it is
+            points += 27 + ord(common[0]) - 65
+        else:
+            points += 1 + ord(common[0]) - 97
 
     print(points)
 
